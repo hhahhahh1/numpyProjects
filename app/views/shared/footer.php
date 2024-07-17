5dc0adf8649bb9e88f7064569ea1e49c0d9a171c
hhahhahh1 <hhahhahh@protonmail.com>
2024-07-17 11:07:59 +0300

    add new utilities

---
+++ app/views/shared/footer.php
@@ -0,0 +1,31 @@
+<footer class="footer mt-auto py-3 sticky-footer">
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-lg-3 gy-2">
+            <div class="col order-last order-lg-first text-light">
+                <div><span class="text-muted">&copy;2022</span> Alexander Nævdal</div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-start justify-content-lg-center"><img src="img/AN-logo-lg-white.png" alt="Alexander Nævdal Logo" height="42"></div>
+            <div class="col d-flex align-items-center justify-content-start justify-content-lg-end">
+                <div class="row">
+                    <!-- <div class="col social"><a href="#" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></div> -->
+                    <div class="col social"><a href="https://github.com/alexnaev" target="_blank"><i class="fab fa-github fa-2x"></i></a></div>
+                </div>
+            </div>
+        </div>
+</footer>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
+<script src="js/site.js"></script>
+<script src="js/prism.js"></script>
+<script>
+    Prism.plugins.NormalizeWhitespace.setDefaults({
+        'remove-trailing': true,
+        'remove-indent': true,
+        'left-trim': true,
+        'right-trim': true
+    })
+</script>
+</body>
+
+</html>
