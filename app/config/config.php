e42d5855565525040b59e2158f8539527b89492e
hhahhahh1 <hhahhahh@protonmail.com>
2024-07-22 11:07:59 +0300

    add new unit tests

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT (Dynamic links)
+define('URLROOT', 'http://localhost/AvdragsBerserk');
+
+//Sitename
+define('SITENAME', 'MVC Framework');
